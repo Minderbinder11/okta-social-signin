9943be70ceeb70f882fb31b3173f1bdc665407ae
Phil <Phil@Lonestar.local>
2017-06-01 18:00:58 -0700

    JS code working for social authentication with Facebook.  key was in the settings at Facebook

--- server/server.js
+++ server/server.js
@@ -34,3 +34,8 @@ app.get('/authorization-code/callback', (req, res) => {
 });
 
 app.listen(8000, console.log(`Server running on port 8000`));
+
+
+// facebook api authorize url:
+// https://dev-477147.oktapreview.com/oauth2/v1/authorize?idp=0oaans3iedl5Ep68R0h7&client_id={clientId}&response_type={responseType}&response_mode={responseMode}&scope={scopes}&redirect_uri={redirectUri}&state={state}&nonce={nonce}
+// https://dev-477147.oktapreview.com/oauth2/v1/authorize?idp=0oaans3iedl5Ep68R0h7&client_id3adcf8cmodTulxBNWZfS=&response_type=id_token&response_mode=fragment&scope=openid%20email%20profile&redirect_uri=http://localhost:8000/authorization-code/callback&state=someState&nonce=someNonce
