f96ef20d8542bc9b7459b5864ec7173ac75e7f0b
Phil <Phil@Lonestar.local>
2017-06-06 13:19:02 -0700

    basic authentication startegy workign

---
+++ client/js/app.js
@@ -0,0 +1,151 @@
+//app.js
+// var qs = require('querystring');
+
+var orgUrl = 'https://dev-477147.oktapreview.com';
+const clientID = 'zVhWxduDfhCXiE4FJUOK';
+var redirectUrl = 'http://localhost:8000/authorization-code/callback';
+
+
+var oktaSignIn = new OktaSignIn({
+  baseUrl: orgUrl,
+  // OpenID Connect options
+  clientId: clientID,
+  redirectUri: redirectUrl,
+  authParams: {
+    responseType: 'id_token',
+    responseMode: 'okta_post_message',
+    scopes: [ 'openid', 'email', 'profile', 'address', 'phone']
+  },
+  idpDisplay: 'PRIMARY',
+  idps: [{
+		'type': 'FACEBOOK',
+		'id': '0oaaq8fqwoLWjGWqI0h7'
+  }],
+
+});
+
+var renderOktaWidget = function() {
+
+  oktaSessionsMe(function (authenticated) {
+
+    console.log('oktasessionsMe Authenticated', authenticated)
+    showAuthUI(authenticated);
+    if(!authenticated) { 
+
+      oktaSignIn.renderEl(
+      { el: '#sign-in-container' },
+      function (res) {
+
+        if(res.status === 'SUCCESS') {
+          oktaSignIn.tokenManager.add('my_id_token', res);
+
+          // check the issuer
+          if (  res.claims.iss === orgUrl && res.claims.aud === clientID &&
+                res.claims.exp > Date.now()/1000 && res.claims.iat > Date.now()/1000 - 10 ) {
+     	        showAuthUI(true);
+          } 
+
+        } else if (res.status === 'FORGOT_PASSWORD_EMAIL_SENT') {
+          // res.username - value user entered in the forgot password form
+          console.log('User %s sent recovery code via email to reset password', res.username);
+        } else if (res.status === 'UNLOCK_ACCOUNT_EMAIL_SENT') {
+            // res.username - value user entered in the unlock account form
+            console.log('User %s sent recovery code via email to unlock account', res.username);
+        }
+      },
+      function (err) { console.log('in err', err);
+      });
+    } 
+   });     
+}; 
+
+
+var oktaSessionsMe = function (callBack) {
+  $.ajax({
+      type: "GET",
+      dataType: 'json',
+      url: orgUrl + "/api/v1/sessions/me",
+      xhrFields: {
+          withCredentials: true
+      },
+      success: function (data) {
+          console.log("My session: ", data);
+         // console.log(data);
+          sessionStorage.setItem( 'sessionTokenKey' , JSON.stringify(data));
+          return callBack(true);
+      },
+      error: function (textStatus, errorThrown) {
+          console.log('No session is present');
+          return callBack(false);
+      },
+      async: true
+  });
+};
+
+var callSessionsMe = function () {
+    oktaSessionsMe(function (authenticated) {
+        console.log('Is user authenticated? ' + authenticated);
+        return authenticated;
+    });
+};    
+
+var showAuthUI = function (isAuthenticated) {
+    if (isAuthenticated) {
+        console.log("authenticated user - hiding widget");
+        $("#apicall-buttons").show();
+        $('#sign-in-container').hide();
+    }
+    else {
+        console.log("anonymous user - showing widget");
+        $("#apicall-buttons").hide();
+        $('#sign-in-container').show();
+    }
+};   
+
+var closeSession = function (callback) {
+  $.ajax({
+      type: "DELETE",
+      dataType: 'json',
+      url: orgUrl + "/api/v1/sessions/me",
+      xhrFields: {
+          withCredentials: true
+      },
+      success: function (data) {
+          console.log('success deleting session');
+          sessionStorage.removeItem('sessionTokenKey');
+          return callback(true);
+      },
+      error: function (textStatus, errorThrown) {
+          console.log('error deleting session: ' + JSON.stringify(textStatus));
+          console.log(errorThrown);
+          return callback(false);
+      },
+      async: true
+  });
+}; 
+
+$('#btnSignOut').click(function () {
+    console.log('signing out');
+    oktaSessionsMe(function (authenticated) {
+        if (authenticated) {
+            var sessionTokenString = sessionStorage.getItem('sessionTokenKey');
+            if (sessionTokenString) {
+                var sessionToken = JSON.parse(sessionTokenString);
+                var sessionId = sessionToken.id;
+                console.log('closing session ' + sessionId);
+                closeSession(function (success) {
+                    console.log('Is session closed? ' + success);
+                    if (success) {
+                        //showAuthUI(false);
+                        location.reload(false);
+                        //$('#claims').hide();
+
+                    }
+                });
+            }
+        }
+    });
+});
+
+
+renderOktaWidget();
--- server/server.js
+++ server/server.js
@@ -5,7 +5,11 @@ import express 				from 'express';
 import bodyParser 	 	from 'body-parser';
 import session 			 	from 'express-session';
 import cookieParser  	from 'cookie-parser';
+import path 					from 'path';
 
+
+const clientId = 'zVhWxduDfhCXiE4FJUOK';
+const clientSecret = '0KYC0ELnG3Atke5d6PBarrHTRlQFVkZ5m2nt_4lP';
 
 const app = express();
 
@@ -21,21 +25,51 @@ app.use(express.static('client'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/signed-in', (req, res) => {
+app.get('/signed-in', (req, res, next) => {
 
-		console.log('in signed in', req.headers.cookie);
-		res.status = 'SUCCESS';
-		res.json({status: 'SUCCESS'});
+  console.log(req.query);
+
+	var nonce, state;
+	///console.log('in signed in', req.headers.cookie);
+	// i think the cookies are set on the client and come backl from the authorization server
+  if (req.cookies['okta-oauth-nonce'] && req.cookies['okta-oauth-state']) {
+    nonce = req.cookies['okta-oauth-nonce'];
+    state = req.cookies['okta-oauth-state'];
+    //console.log('cookies present', req.cookies['okta-oauth-state'])
+  }
+  else {
+    res.status(401).send('"state" and "nonce" cookies have not been set before the /callback request');
+    return;
+  }
+
+  //console.log('req.query.state: ', req.query.state);
+
+  if (!req.query.state || req.query.state !== state) {
+    res.status(401).send(`Query state "${req.query.state}" does not match cookie state "${state}"`);
+    return;
+  }
+
+
+  const secret = new Buffer(`clientId:clientSecret`, 'utf8').toString('base64');
+
+  console.log('this is the secret', secret);
+
+		var myPath = path.join(__dirname, '../client', 'profile.html');
+		console.log('myPath: ', myPath);
+		res.sendFile(myPath, function (err) {
+	    if (err) {
+	    	console.log('err');
+	      next(err);
+	    } else {
+	      console.log('Sent:');
+	    }
+  	});
+
+		//res.status(200).sendFile();
 });
 
 app.get('/authorization-code/callback', (req, res) => {
-
 	console.log('in authorize-code/callback');
 });
 
 app.listen(8000, console.log(`Server running on port 8000`));
-
-
-// facebook api authorize url:
-// https://dev-477147.oktapreview.com/oauth2/v1/authorize?idp=0oaans3iedl5Ep68R0h7&client_id={clientId}&response_type={responseType}&response_mode={responseMode}&scope={scopes}&redirect_uri={redirectUri}&state={state}&nonce={nonce}
-// https://dev-477147.oktapreview.com/oauth2/v1/authorize?idp=0oaans3iedl5Ep68R0h7&client_id3adcf8cmodTulxBNWZfS=&response_type=id_token&response_mode=fragment&scope=openid%20email%20profile&redirect_uri=http://localhost:8000/authorization-code/callback&state=someState&nonce=someNonce
